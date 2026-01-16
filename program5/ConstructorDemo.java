3ab5caee0bfde9d9fd90b217b96e65a312cc7dee
Naziya Begum <naziyamirza04@gmail.com>
2026-01-16 21:32:57 +0530

    fix: constructors details added

--- program5/ConstructorDemo.java
+++ program5/ConstructorDemo.java
@@ -2,7 +2,7 @@ class ConstructorDemo{
     String name;
     int age;
 
-    //constructor overloading-> linking correct constructor and calling them accordingly.
+    //constructor overloading-> linking correct constructor and calling them accordingly( because all constructor have same name.)
     ConstructorDemo(){
         System.out.println("Default constructor is called");
     }
@@ -25,3 +25,7 @@ class ConstructorDemo{
         ConstructorDemo objC = new ConstructorDemo("Aman");
     }
 }
+
+//constructors are nothing but functions which are called during the creation of objects, they have same
+//name as of the class and do not have return type. we have inbuilt default constructors, if we create 
+//one by ourselves then we our constructors will be used.
