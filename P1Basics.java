5961e10f01f409cb906f88834a7ef89f76c1f6e7
Naziya Begum <naziyamirza04@gmail.com>
2026-01-16 09:59:06 +0530

    working of java program added

--- P1Basics.java
+++ P1Basics.java
@@ -1,12 +1,22 @@
 import java.util.*;
 
-class basics
-
-
 class P1Basics{
     public static void main(String args[]){
+        //Rules for compilation:- 
+        //1.If any class is public then while compiling writing javac filename.java 
+        //2. If it is not public then you can give it any name(not recommended)
 
-        //No explicit conversion so it is typeconversion.
+        //Datatype in java
+        //Non Primitive- Integer, Float, Character, Boolean
+        //Primitive- Byte (1), Short(2), int(4), long(8), float(4), double(8)
+
+        //double is by default type to decimal values and int is default type of integer number. 
+        //for float --> float x = 5.6f;
+        //for long --> long x = 6l;
+
+        //TypeCasting, TypeConversion and TypePromotion.
+
+        //No explicit conversion---> TypeConversion.
         int a = 500;
         long b = a;//possible but vice versa is not possible. 
         System.out.println(b);
@@ -15,7 +25,15 @@ class P1Basics{
         long c = (int)5;
         System.out.println(c); 
 
-        int d = Integer.MAX_VALUE * 2;
-        System.out.println(d);
+        //TypePromotion --> promotion from byte-> short -> int -> long implicitly.
+        Byte x = 10;
+        Byte y = 20;
+        //Byte z = x + y ---> throw compile time error. 
+        int z = x + y;
+        System.out.println(z);
+
+        int w = 6/4.0;//error
+        int u = 6/4; //will give integer.
+        System.out.println(w);
     }
 }
---
+++ program2/ObjectDemo.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class main{
+    
+}
+
+class ObjectDemo{
+    int age; 
+    String name;
+
+    ObjectDemo(int age){
+        this.age = age;
+    }
+
+    ObjectDemo(String name){
+        this.name = name;
+    }
+
+    ObjectDemo(int age, String name){
+        this.age = age;
+        this.name = name;
+    }
+
+    public static void main(String args[]){
+        ObjectDemo obj = new ObjectDemo(30, "SpiderMan");
+        // ObjectDemo-> class (kind of datatype of the varible obj)
+        //obj -> name of obj (reference variable);
+
+        System.out.println(obj.age + " " + obj.name);
+    }
+}
+
