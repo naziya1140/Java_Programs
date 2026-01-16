9832ba573c5f4cb1ffd1bcfb3757b9c5873ed779
Naziya Begum <naziyamirza04@gmail.com>
2026-01-16 21:27:21 +0530

    feat: constructor file is added

---
+++ program5/ConstructorDemo.java
@@ -0,0 +1,27 @@
+class ConstructorDemo{
+    String name;
+    int age;
+
+    //constructor overloading-> linking correct constructor and calling them accordingly.
+    ConstructorDemo(){
+        System.out.println("Default constructor is called");
+    }
+
+    ConstructorDemo(String name, int age){
+        System.out.println("Parameterized constructor is called");
+        this.name = name;
+        this.age = age;
+    }
+
+    ConstructorDemo(String name){
+        System.out.println("If one parameter is passed then constructor with single param is called");
+    }
+
+    public static void main(String[] args) {
+        ConstructorDemo objA = new ConstructorDemo();
+
+        ConstructorDemo objB = new ConstructorDemo("Shraddha", 26);
+
+        ConstructorDemo objC = new ConstructorDemo("Aman");
+    }
+}
