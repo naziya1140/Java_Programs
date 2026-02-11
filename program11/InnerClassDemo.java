c792457120b21eb94d201a5dfc6e0b43ed81aee6
Naziya Begum <naziyamirza04@gmail.com>
2026-02-11 22:25:35 +0530

    added details:

--- program11/InnerClassDemo.java
+++ program11/InnerClassDemo.java
@@ -1,3 +1,4 @@
+// eg. 1. Inner class.
 class A{
     void showDetails(){
         System.out.println("This is outer class A");
@@ -17,14 +18,18 @@ class A{
     }
 }
 
+
+
+//eg. 2
+//Anonymous innerclass.
 abstract class Parent{
     String name;
     abstract void show();//method without implementation.
 }
-//Anonymous innerclass.
 
 public class InnerClassDemo {
     public static void main(String[] args) {
+        //eg. 1
         // B obj = new B(); creating object of inner class like this is not possible.
         // Object of the outer class is necessary to create object of inner class. 
 
@@ -37,14 +42,19 @@ public class InnerClassDemo {
         // innerObj2.showDetails();
 
 
-
-        //Anonymous class --> creating a class while declaring the object.
+        //eg. 2
+        //Anonymous class --> creating a inner class while declaring the object.
         Parent obj = new Parent(){
             void show(){
                 System.out.println("Implemented by anonymous inner class.");
             }
+            void showChild(){
+                System.out.println("Present only in child anonymous class");
+            }
         };
 
         obj.show(); //will show new implemented method by the anonymous class.
+        // obj.showChild(); ---> can't call the one which is only present in child anonymous class
+        // because reference of outer class is used and it is not having that method.
     }    
 }
