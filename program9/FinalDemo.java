0ab22c2e97967acd87fa516a57a534c47d001a3f
Naziya Begum <naziyamirza04@gmail.com>
2026-02-11 06:15:46 +0530

    final uses added

---
+++ program9/FinalDemo.java
@@ -0,0 +1,24 @@
+package program9;
+
+final class Quadrilateral{
+    //this is a final class but I can't extend it.    
+}
+
+class Rhombus{
+    //this is final Method but it can't be overrided.
+    final void printArea(){
+        System.out.println("This is the area of a Rhombus");
+    }
+}
+
+class Square extends Rhombus{
+    //cannot rewrite the method which is final.
+    // void printArea(){
+    //     System.out.println("This is the area of a Sqaure");
+    // }
+}
+
+public class FinalDemo {
+    final double PI = 3.14;
+    //PI = 3.1428; //Attempt to change the value of constant variable.
+}
